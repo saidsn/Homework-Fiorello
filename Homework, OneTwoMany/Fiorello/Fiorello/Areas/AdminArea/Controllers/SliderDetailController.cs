91a7b782c694f40cba53dd866b8a8109a26fae95
saidsn <saidsn@code.edu.az>
2022-10-11 08:58:46 +0400

    Homework-Fiorello

---
+++ Homework, OneTwoMany/Fiorello/Fiorello/Areas/AdminArea/Controllers/SliderDetailController.cs
@@ -0,0 +1,28 @@
+﻿using Fiorello.Data;
+using Fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderDetailController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderDetailController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            SliderDetail Sliderdetail = await _context.SliderDetails.FirstOrDefaultAsync();
+
+            return View(Sliderdetail);
+        }
+      
+    }
+}
